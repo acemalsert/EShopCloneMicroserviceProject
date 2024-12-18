45b5d721792bbd8d4864c3f0307438d3b6d6bbdc
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-12-18 11:45:18 +0300

    Event Bus Base Implemented

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -0,0 +1,10 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscrib<T, TH>() where T:IntegrationEvent where TH : IIntegrationEventHandler<T>;
+    }
+}
